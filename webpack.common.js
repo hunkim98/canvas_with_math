765f79d7ea80584fa6dc3acbf64e8c4ea4a73295
Kim Dong Hun <hunkim98@gmail.com>
2022-09-23 19:30:39 +0900

    Change public path again

--- webpack.common.js
+++ webpack.common.js
@@ -40,5 +40,6 @@ module.exports = {
   output: {
     filename: "[name].bundle.js",
     path: path.resolve(__dirname, "dist"),
+    publicPath: argv.mode === "production" ? "/canvas_with_math/" : "/",
   },
 };
--- webpack.prod.js
+++ webpack.prod.js
@@ -3,7 +3,4 @@ const common = require("./webpack.common.js");
 
 module.exports = merge(common, {
   mode: "production",
-  output: {
-    publicPath: "/canvas_with_math",
-  },
 });
